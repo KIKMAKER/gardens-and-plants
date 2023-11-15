0ad2e35345f858c70cb7b0e8de9fb8cfec7d90e2
Kristen Kennedy <kristen.c.kennedy@gmail.com>
2023-11-15 20:18:07 +0200

    completed for batch 1445

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,20 @@
+class PlantsController < ApplicationController
+  def create
+    @plant = Plant.new(plant_params)
+    @garden = Garden.find(params[:garden_id])
+    @plant.garden = @garden
+    if @plant.save
+      redirect_to garden_path(@garden)
+    else
+      @plants = @garden.plants
+      render "gardens/show", status: :unprocessable_entity
+    end
+
+
+  end
+
+  private
+  def plant_params
+    params.require(:plant).permit(:name, :image_url)
+  end
+end
